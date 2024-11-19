8de390ce9f1c4c25a5efa301b1f9169d50e64c71
nhantran433 <trantnnhan0403@gmail.com>
2024-11-19 11:19:33 +0700

    add code

---
+++ app.py
@@ -0,0 +1,53 @@
+import gradio as gr
+from transformers import AutoModelForVision2Seq, AutoProcessor
+from transformers.image_utils import load_image
+import torch
+from PIL import Image
+from idefics2 import Idefics2FT
+
+id2ft = Idefics2FT()
+model_id = "smishr-18/Idefics2-OCR"
+
+# Set up device
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Load model and processor explicitly on the appropriate device
+model = id2ft._load_model(model_id=model_id) if device.type == "cuda" else AutoModelForVision2Seq.from_pretrained(model_id).to(device)
+processor = AutoProcessor.from_pretrained(model_id, do_image_splitting=False)
+
+def inference(image, text):
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "Explain"},
+                {"type": "image"},
+                {"type": "text", "text": text}
+            ]
+        }
+    ]
+
+    image = Image.fromarray(image)
+
+    # Apply chat template
+    text = processor.apply_chat_template(messages, add_generation_prompt=True)
+    
+    # Prepare inputs
+    inputs = processor(text=[text.strip()], images=[image], return_tensors="pt", padding=True)
+    inputs = {k: v.to(device) for k, v in inputs.items()}
+    
+    # Generate texts
+    generated_ids = model.generate(**inputs, max_new_tokens=500)
+    generated_texts = processor.batch_decode(generated_ids, skip_special_tokens=True)
+    return generated_texts[0]
+
+# Set up Gradio interface
+gr_interface = gr.Interface(
+    fn=inference,
+    inputs=["image", "text"],
+    outputs="text",
+    title="Idefics-OCR Inference",
+    description="Upload an image and enter a prompt to get model inference."
+)
+
+gr_interface.launch(debug=True, share=True)
